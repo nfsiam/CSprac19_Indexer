845f80fbc42fbb1fc244f303c9386ba7e82e8c03
nfsiam <iamthenfs@gmail.com>
2019-10-01 01:25:44 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSprac19_Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School s = new School();
+
+            //printing names using id
+            Console.WriteLine(s[1]);
+            Console.WriteLine(s[3]);
+
+            //changing names through id
+            s[1] = "Changed Siam";
+            s[3] = "Changed Tamim";
+
+            //printing names again after changing
+            Console.WriteLine(s[1]);
+            Console.WriteLine(s[3]);
+
+            //printing department names using names
+            Console.WriteLine(s["Habib"]);
+            Console.WriteLine(s["Ahad"]);
+
+            //changing department using name
+            s["Habib"] = "BBA";
+            s["Ahad"] = "CSE";
+
+            //again printing department names using names
+            Console.WriteLine(s["Habib"]);
+            Console.WriteLine(s["Ahad"]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ School.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSprac19_Indexer
+{
+    class School
+    {
+        private List<Student> studentList = new List<Student>();
+        public School()
+        {
+            studentList.Add(new Student { ID = 1, Department = "CSE", Name = "Siam", });
+            studentList.Add(new Student { ID = 2, Department = "CSE", Name = "Nafiz", });
+            studentList.Add(new Student { ID = 3, Department = "BBA", Name = "Tamim", });
+            studentList.Add(new Student { ID = 4, Department = "BBA", Name = "Ahad", });
+            studentList.Add(new Student { ID = 5, Department = "EEE", Name = "Habib", });
+            studentList.Add(new Student { ID = 6, Department = "EEE", Name = "Prodip", });
+        }
+
+        public string this[int id]
+        {
+            get
+            {
+                foreach(Student s in studentList)
+                {
+                    if(s.ID==id)
+                    {
+                        return s.Name;
+                    }
+                }
+                return "Invalid ID";
+            }
+            set
+            {
+                foreach (Student s in studentList)
+                {
+                    if (s.ID == id)
+                    {
+                        s.Name = value;
+                    }
+                }
+            }
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                foreach (Student s in studentList)
+                {
+                    if (s.Name.Equals(name))
+                    {
+                        return s.Department;
+                    }
+                }
+                return "Invalid ID";
+            }
+            set
+            {
+                foreach (Student s in studentList)
+                {
+                    if (s.Name.Equals(name))
+                    {
+                        s.Department = value;
+                    }
+                }
+            }
+        }
+    }
+}
